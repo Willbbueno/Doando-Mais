7917f757478ff81cad1c11d4b71ea465ac4afa39
William Bueno <will.b.bueno@gmail.com>
2025-11-01 15:19:01 -0300

    Atualiza e cria estrutura das classes authRepository e userRepository

--- app/src/main/java/com/upx/doando_mais/repository/AuthRepository.java
+++ app/src/main/java/com/upx/doando_mais/repository/AuthRepository.java
@@ -1,4 +1,56 @@
 package com.upx.doando_mais.repository;
-// classe que lida Login, cadastro, logout via firestore auth
+// classe que lida Login, cadastro, logout via firestore auth, cuida de toda a autenticação.
+
+import com.google.firebase.auth.FirebaseAuth;
+import androidx.lifecycle.MutableLiveData; // Importante
+import com.google.firebase.auth.FirebaseUser;
+
 public class AuthRepository {
+
+    private FirebaseAuth firebaseAuth;
+    private MutableLiveData<FirebaseUser> usuarioLogadoLiveData; // Informa quem está logado
+    private MutableLiveData<String> erroAutenticacaoLiveData; // Informa se deu erro
+    private MutableLiveData<Boolean> cadastroSucessoLiveData; // Informa se o cadastro deu certo
+
+    public AuthRepository() {
+        this.firebaseAuth = FirebaseAuth.getInstance();
+        this.usuarioLogadoLiveData = new MutableLiveData<>();
+        this.erroAutenticacaoLiveData = new MutableLiveData<>();
+        this.cadastroSucessoLiveData = new MutableLiveData<>();
+
+        // Primeiro verifica se já existe um usuário logado ao iniciar o app
+        if (firebaseAuth.getCurrentUser() != null) {
+            usuarioLogadoLiveData.postValue(firebaseAuth.getCurrentUser());
+        }
+    }
+
+    // Métodos para autenticação - LOGIN, CADASTRAR E LOGOUT
+    public void login(String email, String senha) {
+
+    }
+
+    // --- Método de Cadastro ---
+    public void cadastrar(String email, String senha) {
+
+    }
+
+    // --- Método de Logout ---
+    public void logout() {
+        firebaseAuth.signOut();
+        usuarioLogadoLiveData.postValue(null); // Avisa que o usuário deslogou
+    }
+
+
+
+    public MutableLiveData<FirebaseUser> getUsuarioLogadoLiveData() {
+        return usuarioLogadoLiveData;
+    }
+
+    public MutableLiveData<String> getErroAutenticacaoLiveData() {
+        return erroAutenticacaoLiveData;
+    }
+
+    public MutableLiveData<Boolean> getCadastroSucessoLiveData() {
+        return cadastroSucessoLiveData;
+    }
 }
--- app/src/main/java/com/upx/doando_mais/repository/UserRepository.java
+++ app/src/main/java/com/upx/doando_mais/repository/UserRepository.java
@@ -1,4 +1,49 @@
 package com.upx.doando_mais.repository;
-// classe que lida com dados do usuário no firestore.
+
+// vai interagir com a coleção Usuários no firestore
+import androidx.lifecycle.MutableLiveData;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.upx.doando_mais.data.model.Usuario;
+
 public class UserRepository {
+
+    private static final String COLLECTION_NAME = "usuarios";
+    private FirebaseFirestore db;
+    private CollectionReference usuariosCollection;
+
+    private MutableLiveData<Usuario> usuarioLiveData; // Informa os dados do usuário buscado
+    private MutableLiveData<String> erroLiveData; // Informa se deu erro
+    private MutableLiveData<Boolean> salvamentoSucessoLiveData; // Informa se o salvamento deu certo
+
+    public UserRepository() {
+        this.db = FirebaseFirestore.getInstance();
+        this.usuariosCollection = db.collection(COLLECTION_NAME);
+
+        this.usuarioLiveData = new MutableLiveData<>();
+        this.erroLiveData = new MutableLiveData<>();
+        this.salvamentoSucessoLiveData = new MutableLiveData<>();
+    }
+
+    //Salva os dados adicionais do usuário no Firestore após o cadastro.
+    public void salvarUsuarioAdicional(String uid, Usuario usuario) {
+    }
+
+    //Busca os dados de um usuário no Firestore usando seu UID.
+
+    public void buscarUsuario(String uid) {
+
+    }
+
+    public MutableLiveData<Usuario> getUsuarioLiveData() {
+        return usuarioLiveData;
+    }
+
+    public MutableLiveData<String> getErroLiveData() {
+        return erroLiveData;
+    }
+
+    public MutableLiveData<Boolean> getSalvamentoSucessoLiveData() {
+        return salvamentoSucessoLiveData;
+    }
 }
