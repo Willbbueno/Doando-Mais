d887a71d0e86812e4498e86eda190523a29dbe1a
William Bueno <will.b.bueno@gmail.com>
2025-11-02 20:35:12 -0300

    Cria FeedViewModel - guarda os dados para a tela do feed

--- app/src/main/java/com/upx/doando_mais/ui/feed/FeedViewModel.java
+++ app/src/main/java/com/upx/doando_mais/ui/feed/FeedViewModel.java
@@ -1,4 +1,47 @@
 package com.upx.doando_mais.ui.feed;
-// Tela principal do feed de campanhas
-public class FeedViewModel {
+// classe para criar uma instancia do campaignRepository e deixar observável os livedatas para o feedfragment. Na prática irá guardar o dado para a tela.
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModel;
+import com.upx.doando_mais.data.model.Campanha;
+import com.upx.doando_mais.repository.CampaignRepository; // Importe o repositório
+
+import java.util.List;
+
+public class FeedViewModel extends ViewModel {
+
+    private CampaignRepository campaignRepository;
+
+    // LiveData que o Fragment vai observar
+    private LiveData<List<Campanha>> campanhasLiveData;
+    private LiveData<String> erroLiveData;
+
+    // Construtor
+    public FeedViewModel() {
+        // 1. Cria a instância do repositório
+        campaignRepository = new CampaignRepository();
+
+        // 2. "Pega emprestado" os LiveData do repositório
+        campanhasLiveData = campaignRepository.getCampanhasLiveData();
+        erroLiveData = campaignRepository.getErroLiveData();
+    }
+
+    // --- Getters para o Fragment ---
+
+    public LiveData<List<Campanha>> getCampanhasLiveData() {
+        return campanhasLiveData;
+    }
+
+    public LiveData<String> getErroLiveData() {
+        return erroLiveData;
+    }
+
+    // --- Ação ---
+
+    /**
+     * Pede ao repositório para começar a buscar as campanhas.
+     * O Fragment chamará este método.
+     */
+    public void carregarCampanhas() {
+        campaignRepository.buscarTodasCampanhas();
+    }
 }
