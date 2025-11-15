b4e68df471b7c3aa67c54fd28b02e6e0c19bc68d
William Bueno <will.b.bueno@gmail.com>
2025-11-15 12:47:50 -0300

    Cria tela orientações

--- app/src/main/java/com/upx/doando_mais/ui/orientacoes/OrientacoesFragment.java
+++ app/src/main/java/com/upx/doando_mais/ui/orientacoes/OrientacoesFragment.java
@@ -1,11 +1,40 @@
 package com.upx.doando_mais.ui.orientacoes;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import com.upx.doando_mais.databinding.FragmentOrientacoesBinding; // Importe o Binding
 
-// tela de orientações, será estática, apenas para informações
 public class OrientacoesFragment extends Fragment {
 
-    // Construtor vazio
-    public OrientacoesFragment(){}
+    private FragmentOrientacoesBinding binding;
 
+    public OrientacoesFragment() {
+        // Required empty public constructor
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // Infla o layout usando ViewBinding
+        binding = FragmentOrientacoesBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        // Nenhuma lógica de Java é necessária aqui,
+        // pois o layout XML é 100% estático.
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null; // Limpa a referência do binding
+    }
 }
